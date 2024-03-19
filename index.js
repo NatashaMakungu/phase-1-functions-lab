f68f4e863e62280abea0f09715f4b8535071fc37
Natasha Makungu <natasha.makungu@student.moringaschool.com>
2024-03-19 19:33:00 +0300

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  const hq = 42;  
+  const distance = Math.abs(hq - block);  
+  return distance;
+}
+
+function distanceFromHqInFeet(block) {
+  const hq = 42;  
+  const feetPerBlock = 264;  
+  const distance = Math.abs(block - hq) * feetPerBlock;
+  return distance;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const feetPerBlock = 264;  
+  const distance = Math.abs(endBlock - startBlock) * feetPerBlock;
+  return distance;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+  const feetPerBlock = 264;  
+  const freeSampleDistance = 400;  
+  const minChargeableDistance = 400;  
+  const maxChargeableDistance = 2000;  
+  const flatRateCharge = 25;  
+  const distance = Math.abs(endBlock - startBlock) * feetPerBlock;
+  let farePrice;
+  if (distance <= freeSampleDistance) {
+      farePrice = 0;  
+  } else if (distance <= maxChargeableDistance) {
+      farePrice = (distance - freeSampleDistance) * 0.02;  
+  } else if (distance > maxChargeableDistance && distance <= 2500) {
+      farePrice = flatRateCharge;  
+  } else {
+      farePrice = "cannot travel that far";  
+  }
+
+  return farePrice;
+}
